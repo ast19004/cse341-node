fba1cd503103b01920d1febde134eb1ece17d258
Anna Astle <ast19004@byui.edu>
2022-01-07 16:15:04 -0800

    01 Prove: Assignment

---
+++ section4/app.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const fs = require('fs');
+const { parse } = require('path/posix');
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method = 'POST';
+
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form method="POST" action="/message"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+    }
+   res.setHeader('Content-Type', 'text/html');
+   res.write('<html>');
+   res.write('<head><title>My First Page</title></head>');
+   res.write('<body><h1>Hello from my Node.js Server</h1></body>');
+   res.write('</html>'); 
+});
+
+server.listen(4000);
+
+
